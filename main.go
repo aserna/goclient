405cbf803e6dba8e12c86a42eac4fca6e5165946
adrian serna <adrianserna@MacBook-Air-de-adrian.local>
2020-08-23 15:13:00 -0500

    go cli

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	printUser := false
+
+	flag.BoolVar(&printUser, "print-user", true, "Print a name if set to true")
+
+	flag.Parse()
+
+	user := os.Getenv("USER")
+
+	if printUser {
+		fmt.Println("Hello " + user)
+
+		fmt.Printf("Hello %s\n", user)
+	}
+}
